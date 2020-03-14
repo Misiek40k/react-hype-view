f9bb5be14bbb0a87133ddc249f6ab5f40562feab
Misiek40k <comanderrr@gmail.com>
2020-03-14 16:07:40 +0100

    Add Icon component

---
+++ src/components/common/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Logo.module.scss';
+
+const Logo = () => {
+  return (
+    <div className={styles.component}>
+
+    </div>
+  );
+};
+
+export default Logo;

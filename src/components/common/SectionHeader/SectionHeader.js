03927b0c7148c86edc368dea5a37e445c67f1384
Misiek40k <comanderrr@gmail.com>
2020-03-14 21:36:36 +0100

    Add SectionHeader

---
+++ src/components/common/SectionHeader/SectionHeader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './SectionHeader.module.scss';
+
+const SectionHeader = () => (
+  <h1 className={styles.header}>People</h1>
+);
+
+export default SectionHeader;
--- src/components/layout/ContentContainer/ContentContainer.js
+++ src/components/layout/ContentContainer/ContentContainer.js
@@ -1,12 +1,15 @@
 import React  from 'react';
 
+import SectionHeader from '../../common/SectionHeader/SectionHeader';
+
 import styles from './ContentContainer.module.scss';
+
 
 const ContentContainer = () => {
 
   return (
     <section className={styles.component}>
-      content
+      <SectionHeader/>
     </section>
   );
 };

922e5d698a07dcfeee0e6d97012c70bc6ae02f04
Misiek40k <comanderrr@gmail.com>
2020-03-15 22:22:23 +0100

    Add danger button variant

--- src/components/features/InnerContainer/InnerContainer.js
+++ src/components/features/InnerContainer/InnerContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { settings } from '../../../data/dataStore';
 
 import Title from '../../common/Title/Title';
 import Button from '../../common/Button/Button';
@@ -6,10 +7,12 @@ import Button from '../../common/Button/Button';
 import styles from './InnerContainer.module.scss';
 
 const InnerContainer = props => {
+  const data = settings.content.buttons;
+
   return (
     <div className={styles.component}>
       <Title subtitle='Age 40+' />
-      <Button />
+      <Button variant={`${data.size.small} ${data.variant.danger}`} name={data.icon.minus} />
     </div>
   );
 };

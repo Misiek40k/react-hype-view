5d49059db436557cdabd5c562a07eec24648b5ba
Misiek40k <comanderrr@gmail.com>
2020-03-17 18:54:48 +0100

    Add ItemOption component

--- src/components/common/ItemOption/ItemOption.js
+++ src/components/common/ItemOption/ItemOption.js
@@ -3,13 +3,16 @@ import PropTypes from 'prop-types';
 import { settings } from '../../../data/dataStore';
 
 import Button from '../Button/Button';
+import Title from '../Title/Title';
+
+import styles from './ItemOption.module.scss';
 
 const ItemOption = ({ option }) => {
   const data = settings.content.buttons;
 
   return (
-    <div>
-      <span>{option}</span>
+    <div className={styles.component}>
+      <Title subtitle={option} />
       <Button
         variant={`${data.size.small} ${data.variant.danger}`}
         name={data.icon.minus}
